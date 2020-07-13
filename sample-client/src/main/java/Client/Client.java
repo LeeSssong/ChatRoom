4c4a43bc94c9fd4f91b669e0dda197e5ddabf21e
LeeSssong <940310446@qq.com>
2020-07-13 23:59:03 +0800

    更改服务器处理客户端逻辑，使得每一个客户端都是独立的一个线程，并模拟高并发状态添加了一个测试类。

--- sample-client/src/main/java/Client/Client.java
+++ sample-client/src/main/java/Client/Client.java
@@ -3,7 +3,7 @@ package Client;
 
 import Client.bean.ServerInfo;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Client {
     public static void main(String[] args) {
@@ -11,11 +11,35 @@ public class Client {
         System.out.println("Server:" + info);
 
         if (info != null) {
+            TCPClient tcpClient = null;
             try {
-                TCPClient.linkWith(info);
+                tcpClient = TCPClient.startWith(info);
+                if (tcpClient == null)
+                    return;
+                // 键盘输出
+                write(tcpClient);
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                if (tcpClient != null)
+                    tcpClient.exit();
             }
         }
+    }
+    private static void write(TCPClient tcpClient) throws IOException {
+        // 构建键盘输入流
+        InputStream in = System.in;
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+        do {
+            // 键盘读取一行
+            String str = input.readLine();
+            // 发送到服务器
+            tcpClient.send(str);
+
+            if ("00bye00".equalsIgnoreCase(str)) {
+                break;
+            }
+        } while (true);
     }
 }
